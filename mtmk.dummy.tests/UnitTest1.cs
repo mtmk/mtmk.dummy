a8f9c71dac7eefc5194f0de2e1bf331f0ae03858
Ziya Suzen <ziya@suzen.net>
2024-06-27 17:29:26 +0100

    Add a reference to mtmk.dummy in mtmk.dummy.tests
    This commit adds a project reference to the mtmk.dummy project in the mtmk.dummy.tests project file. It also adds a line to create an instance of SimpleMaths and print the result of an addition operation in the Test1 method.

--- mtmk.dummy.tests/UnitTest1.cs
+++ mtmk.dummy.tests/UnitTest1.cs
@@ -7,6 +7,8 @@ public class UnitTest1(ITestOutputHelper @out)
     [Fact]
     public void Test1()
     {
+        SimpleMaths simpleMaths = new();
+        @out.WriteLine($"{simpleMaths.Add(1, 2)}");
         @out.WriteLine($"{Environment.Version}");
     }
 }
