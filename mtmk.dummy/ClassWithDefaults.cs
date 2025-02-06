07543568d8ceef9301376ea0868c2aa7ba735c63
Ziya Suzen <ziya@suzen.net>
2025-02-06 11:04:11 +0000

    Update Sqr methods to support optional sqrt calculation
    Added a boolean parameter to `Sqr` methods in both `ClassWithDefaults` and `ClassWithOverloads` to conditionally calculate the square root. Updated the version number to 4.0.1 to reflect these modifications.

--- mtmk.dummy/ClassWithDefaults.cs
+++ mtmk.dummy/ClassWithDefaults.cs
@@ -2,13 +2,16 @@
 
 public interface IClassWithDefaults
 {
-    int Sqr(int arg1);
+    int Sqr(int arg1, bool arg2 = false);
 }
 
 public class ClassWithDefaults : IClassWithDefaults
 {
-    public int Sqr(int arg1)
+    public int Sqr(int arg1, bool arg2 = false)
     {
+        if (arg2)
+            return (int)Math.Sqrt(arg1);
+
         return arg1 * arg1;
     }
 }
@@ -16,12 +19,21 @@ public class ClassWithDefaults : IClassWithDefaults
 public interface IClassWithOverloads
 {
     int Sqr(int arg1);
+    int Sqr(int arg1, bool arg2);
 }
 
 public class ClassWithOverloads : IClassWithOverloads
 {
     public int Sqr(int arg1)
     {
+        return arg1 * arg1;
+    }
+
+    public int Sqr(int arg1, bool arg2)
+    {
+        if (arg2)
+            return (int)Math.Sqrt(arg1);
+        
         return arg1 * arg1;
     }
 }
