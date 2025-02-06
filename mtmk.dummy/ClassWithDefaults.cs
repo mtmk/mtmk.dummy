a992f10f4932758750af4ffb5245f194c930c6f6
Ziya Suzen <ziya@suzen.net>
2025-02-06 10:55:26 +0000

    Init class overloads

---
+++ mtmk.dummy/ClassWithDefaults.cs
@@ -0,0 +1,27 @@
+﻿namespace mtmk.dummy;
+
+public interface IClassWithDefaults
+{
+    int Sqr(int arg1);
+}
+
+public class ClassWithDefaults : IClassWithDefaults
+{
+    public int Sqr(int arg1)
+    {
+        return arg1 * arg1;
+    }
+}
+
+public interface IClassWithOverloads
+{
+    int Sqr(int arg1);
+}
+
+public class ClassWithOverloads : IClassWithOverloads
+{
+    public int Sqr(int arg1)
+    {
+        return arg1 * arg1;
+    }
+}
